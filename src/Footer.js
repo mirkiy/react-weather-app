16823ce993e32837791b069b14429b30a197d7a6
mirkiy <77293514+mirkiy@users.noreply.github.com>
2021-03-27 14:34:02 +0000

    Updated link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,11 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <small>
-      <a href="https://github.com/mirkiy/react-weather-app" target="_blank">
+      <a
+        href="https://github.com/mirkiy/react-weather-app"
+        target="_blank "
+        rel="noreferrer"
+      >
         Open-source code
       </a>
       , by Mirka Zacharova
