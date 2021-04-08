e687fd353d19586141f6ec62cb5a15df2138f09a
mirkiy <77293514+mirkiy@users.noreply.github.com>
2021-04-08 10:49:36 +0100

    Current location button fixed

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./Search.css";
 import Overview from "./Overview";
-
 import axios from "axios";
 import Forecast from "./Forecast";
 
@@ -10,6 +9,20 @@ export default function Search(props) {
   const [city, setCity] = useState(props.defaultCity);
   const [unit, setUnit] = useState("celsius");
 
+  //Geolocation - current location btn
+  function currentLocation(response) {
+    let latitude = response.coords.latitude;
+    let longitude = response.coords.longitude;
+    let apiKey = "c00c3900e504f42a171bf56d7e9488fa";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function getLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(currentLocation);
+  }
+
+  //Handle function for the API's Response
   function handleResponse(response) {
     setWeatherData({
       ready: true,
@@ -28,16 +41,21 @@ export default function Search(props) {
     event.preventDefault();
     searching();
   }
+
+  //Handling the city input
   function handleCityChange(event) {
+    event.preventDefault();
     setCity(event.target.value);
   }
 
+  //Searching the API
   function searching() {
     const apiKey = "c00c3900e504f42a171bf56d7e9488fa";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  //Return
   if (weatherData.ready) {
     return (
       <div className="Search">
@@ -60,6 +78,7 @@ export default function Search(props) {
                 xmlns="http://www.w3.org/2000/svg"
                 className="bi bi-geo-alt city-current"
                 viewBox="0 0 16 16"
+                onClick={getLocation}
               >
                 <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
                 <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
@@ -72,6 +91,7 @@ export default function Search(props) {
       </div>
     );
   } else {
+    //Call the API
     searching();
     return "Loading...";
   }
